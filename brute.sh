3e723a5168452012a7d2e4d58562ab221093df9d
Anonymous <83106526+baron8899@users.noreply.github.com>
2021-05-12 18:57:18 +0700

    Update brute.sh

--- brute.sh
+++ brute.sh
@@ -1,8 +1,16 @@
 #!bin/sh
+##banner
 banner() {
-        cat <<- EOF
-                ${RED}_____________________
-
-        EOF
-        
+	cat <<- EOF
+		${ORANGE}
+		${ORANGE}           
+		${ORANGE}          
+		${ORANGE} 
+		${ORANGE}
+		${ORANGE} 
+		${ORANGE}
+		${ORANGE}      | |                                
+		${ORANGE}      |_|                ${RED}Version : 2.1
+		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by htr-tech (tahmid.rayat)${WHITE}
+	EOF
 }
