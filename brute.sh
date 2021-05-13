8bf0535d7fa12e4c557d38572e498fa8214aeffb
Anonymous <83106526+baron8899@users.noreply.github.com>
2021-05-13 10:39:32 +0700

    Update brute.sh

--- brute.sh
+++ brute.sh
@@ -2,15 +2,19 @@
 ##banner
 banner() {
 	cat <<- EOF
-		${ORANGE}
-		${ORANGE}           
-		${ORANGE}          
-		${ORANGE} 
-		${ORANGE}
-		${ORANGE} 
-		${ORANGE}
-		${ORANGE}      | |                                
-		${ORANGE}      |_|                ${RED}Version : 2.1
-		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by htr-tech (tahmid.rayat)${WHITE}
+		${ORANGE}   /\  | | /\ ||
+		${ORANGE}   / \ | | / \ |       
+		${ORANGE}  |  | |   / / \
+		${ORANGE} \ | | | / | \ |
+		${ORANGE} | / | | | | | |
+		${ORANGE} | |   | |   | | 
+		${ORANGE}     | |  | | |
+		${ORANGE}      | |   | |                            
+		${ORANGE}      |_|    |            ${RED}Version : 2.1
+		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by Anonymous FL team (BaoLam and Tu)${WHITE}
 	EOF
+}
+##print
+ex() {
+   {banner}
 }
